dd4b083d05367450f2b887a4389681fdebb2a017
christopherfricke <christopher.fricke@bmoregeo.com>
2014-06-07 07:36:45 -0400

    remove extra extension call

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,13 +4,6 @@ execute :create_postgis_database
 	action :run
 end
 
-execute :add_postgis_extension do
-	user "postgres"
-	command "psql -d #{node[:postgis][:database_name]}  -c 'CREATE EXTENSION postgis'"
-	not_if "psql -qAt --list | grep '^#{node[:postgis][:template_name]}\|'", :user => 'postgres'
-	action :run
-end
-
 for extension in ["postgis", "postgis_topology", "fuzzystrmatch", "postgis_tiger_geocoder"] do
 	execute "Install #{extension}" do
 		user "postgres"
