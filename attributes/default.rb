8d61dfd2b27bf03e0df08d0ae553e84015abd2c2
christopherfricke <christopher.fricke@bmoregeo.com>
2014-06-07 08:37:33 -0400

    clean up checking if db exists

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,3 @@
 default[:postgis][:database_name] = "postgis"
+default[:postgis][:postgresql_version] = "9.3"
+default[:postgis][:postgis_version] = "2.1"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,18 @@
+package "postgresql-#{node[:postgis][:postgresql_version]}-postgis-#{node[:postgis][:postgis_version]}"
+
 execute :create_postgis_database do
 	user "postgres"
-	command "createdb -E UTF8 --locale=en_US.utf8 #{node[:postgis][:database_name]}"
+	command "createdb #{node[:postgis][:database_name]} -E UTF8"
 	action :run
+	not_if "psql -qAt --list | grep '^#{node['postgis']['database_name']}\|'", user: "postgres"
 end
 
 for extension in ["postgis", "postgis_topology", "fuzzystrmatch", "postgis_tiger_geocoder"] do
+	execute "Drop #{extension}" do
+		user "postgres"
+		command "psql -d #{node[:postgis][:database_name]}  -c 'DROP EXTENSION IF EXISTS #{extension}'"
+		action :run
+	end
 	execute "Install #{extension}" do
 		user "postgres"
 		command "psql -d #{node[:postgis][:database_name]}  -c 'CREATE EXTENSION #{extension}'"
